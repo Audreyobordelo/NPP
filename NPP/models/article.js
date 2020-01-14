6a07278f860087c3e7f6d753a8ccc247e0fefd90
samo <audreychevron@gmail.com>
2020-01-14 19:53:09 +0100

    2e commit

--- NPP/models/article.js
+++ NPP/models/article.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const ArticleSchema = Schema({
+  mediaId: {type: Schema.Types.ObjectId, ref: 'Media'},
   title: String,
   picFeatured: String,
   picCaption: String,
   author: String,
   article: String,
   tags: Array
+}, {
+  timestamps: true
 });
 
 const Article = mongoose.model('Article', ArticleSchema);
