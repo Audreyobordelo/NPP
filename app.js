ea3015428322328210d87c662d40b89a8216a910
Ahlem Khattab <ahlemkhattab@gmail.com>
2020-01-16 20:51:03 +0100

    heroku

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const flash = require("connect-flash");
 const Media = require('./models/media.js');
 
 mongoose
-  .connect('mongodb://localhost/npp', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
